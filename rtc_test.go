01b7fc51b5b0866dc3f3e928e60c93e304ab0588
Andrey Petrov <andrey.petrov@shazow.net>
2020-11-10 16:33:53 -0500

    wip: Trying to use rtcServer in rtc_test, does not work yet

--- rtc_test.go
+++ rtc_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"io"
 	"testing"
 	"time"
 
@@ -11,62 +10,110 @@ import (
 )
 
 func TestRTC(t *testing.T) {
-	s := webrtc.SettingEngine{}
-	api := webrtc.NewAPI(webrtc.WithSettingEngine(s))
+	srv := rtcServer{}
+	srv.init()
+	api := srv.api
 
 	offerPC, err := api.NewPeerConnection(webrtc.Configuration{})
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer offerPC.Close()
 
-	answerPC, err := api.NewPeerConnection(webrtc.Configuration{})
+	offerSession, err := prepareOffer(offerPC)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal("failed to prepare offer", err)
 	}
 
-	defer closePairNow(t, offerPC, answerPC)
+	ready := make(chan struct{})
 
-	answerPC.OnDataChannel(func(d *webrtc.DataChannel) {
-		d.OnMessage(func(msg webrtc.DataChannelMessage) {
-			e := d.Send([]byte("Pong"))
-			if e != nil {
-				t.Fatalf("Failed to send string on data channel")
-			}
-		})
-		t.Log("answerPC.OnDataChannel", d)
+	offerPC.OnDataChannel(func(d *webrtc.DataChannel) {
+		t.Log("offerPC.OnDataChannel", d)
 	})
-
-	offerDC, err := offerPC.CreateDataChannel("foo", nil)
+	offerDC, err := offerPC.CreateDataChannel("test-data-channel", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	if err := signalPair(offerPC, answerPC); err != nil {
-		t.Fatal(err)
-	}
-
-	t.Log("Waiting for data channel to open")
-	open := make(chan struct{})
 	offerDC.OnOpen(func() {
-		open <- struct{}{}
+		t.Log("offerDC.OnOpen")
+		ready <- struct{}{}
 	})
-	<-open
-	t.Log("data channel opened")
+
+	// rtcServer.accept handles:
+	// * conn := NewPeerConnection
+	// * conn.SetRemoteDescription(offer)
+	// * answer := conn.CreateAnswer(nil)
+	// * conn.SetLocalDescription(answer)
+	rtcConn, err := srv.accept(*offerSession)
+	if err != nil {
+		t.Fatal("failed to accept rtcServer offer", err)
+	}
+	defer rtcConn.Close()
+
+	// Acquire answer
+	answer := rtcConn.LocalDescription()
+	if err = offerPC.SetRemoteDescription(*answer); err != nil {
+		t.Fatal(err)
+	}
+	t.Log("offer: ", offerSession)
+	t.Log("answer: ", answer)
+
+	t.Log("waiting for data channel")
+	rtcConn.OnDataChannel(func(dc *webrtc.DataChannel) {
+		t.Log("rtcConn.OnDataChannel", dc)
+		dc.OnMessage(func(msg webrtc.DataChannelMessage) {
+			t.Log("rtcConn.dc.OnMessage", dc)
+			dc.Send([]byte("Pong"))
+		})
+	})
 
 	received := make(chan []byte)
 	offerDC.OnMessage(func(msg webrtc.DataChannelMessage) {
+		t.Log("offerDC.OnMessage", msg)
 		received <- msg.Data
 	})
+
+	<-ready
 	if err := offerDC.Send([]byte("Ping")); err != nil {
 		t.Fatal(err)
 	}
 
+	t.Log("waiting for pong")
 	if got, want := <-received, []byte("Pong"); !bytes.Equal(got, want) {
 		t.Errorf("got: %s; want: %s", got, want)
 	} else {
 		t.Logf("received response: %s", got)
 	}
 
+}
+
+// prepareOffer sets up the signal of the offer side of the peer connection.
+//
+// Based on https://github.com/pion/webrtc/blob/v2/peerconnection_test.go#L31
+//
+// Note: If this ever needs to run in js/wasm, we'll need to create a dummy
+// datachannel to trigger ICE gathering.
+func prepareOffer(pc *webrtc.PeerConnection) (*webrtc.SessionDescription, error) {
+	iceGatheringState := pc.ICEGatheringState()
+	offerCh := make(chan *webrtc.SessionDescription, 1)
+
+	if iceGatheringState != webrtc.ICEGatheringStateComplete {
+		pc.OnICECandidate(func(candidate *webrtc.ICECandidate) {
+			if candidate == nil {
+				offerCh <- pc.PendingLocalDescription()
+			}
+		})
+	}
+
+	if offer, err := pc.CreateOffer(nil); err != nil {
+		return nil, err
+	} else if err := pc.SetLocalDescription(offer); err != nil {
+		return nil, err
+	} else if iceGatheringState == webrtc.ICEGatheringStateComplete {
+		return &offer, nil
+	}
+
+	return <-offerCh, nil
 }
 
 // signalPair is borrowed from https://github.com/pion/webrtc/blob/v2/peerconnection_test.go
@@ -122,20 +169,5 @@ func signalPair(pcOffer *webrtc.PeerConnection, pcAnswer *webrtc.PeerConnection)
 			return err
 		}
 		return nil
-	}
-}
-
-func closePairNow(t *testing.T, pc1, pc2 io.Closer) {
-	var fail bool
-	if err := pc1.Close(); err != nil {
-		t.Errorf("Failed to close PeerConnection: %v", err)
-		fail = true
-	}
-	if err := pc2.Close(); err != nil {
-		t.Errorf("Failed to close PeerConnection: %v", err)
-		fail = true
-	}
-	if fail {
-		t.FailNow()
 	}
 }
