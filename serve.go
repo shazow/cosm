1f1d12cab45f1dfabdd69d76b24eba1eb621c1ff
Andrey Petrov <andrey.petrov@shazow.net>
2020-11-18 13:31:03 -0500

    web demo connects, receives message

--- serve.go
+++ serve.go
@@ -23,9 +23,11 @@ func serve(ctx context.Context, options Options) error {
 		Logger: logger,
 		HandleConnection: func(conn rtcConn) {
 			logger.Info().Interface("conn", conn).Msg("new connection")
-			if err := conn.DataChannel.SendText("hello"); err != nil {
-				logger.Error().Err(err).Msg("data channel send failed")
-			}
+			conn.DataChannel.OnOpen(func() {
+				if err := conn.DataChannel.SendText("hello"); err != nil {
+					logger.Error().Err(err).Msg("data channel send failed")
+				}
+			})
 		},
 	}
 	rtc.init()
